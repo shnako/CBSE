558eddef6ccc8e71e5964e3b2af0d1ebf4ede7ef
1106695s <1106695s@student.gla.ac.uk>
2015-01-21 14:39:19 +0000

    Added Registry implementation.

--- AX1/mCom/src/org/gla/mcom/impl/RegistryImpl.java
+++ AX1/mCom/src/org/gla/mcom/impl/RegistryImpl.java
@@ -2,18 +2,37 @@ package org.gla.mcom.impl;
 
 import org.gla.mcom.Registry;
 
+import java.util.HashSet;
+
 public class RegistryImpl implements Registry{
+	private static RegistryImpl registrySingleton;
+	private HashSet<String> ip_ports;
+
+	private RegistryImpl() {
+		ip_ports = new HashSet<String>();
+
+	}
+
+	public static RegistryImpl getRegistry() {
+		if (registrySingleton != null) {
+			return registrySingleton;
+		}
+
+		return new RegistryImpl();
+	}
 
 	@Override
 	public String[] lookup() {
-		// TODO Auto-generated method stub
-		return null;
+		return (String[])ip_ports.toArray();
 	}
 
 	@Override
 	public boolean register(String ip_port) {
-		// TODO Auto-generated method stub
-		return false;
+		return ip_ports.add(ip_port);
 	}
 
+	@Override
+	public boolean deregister(String ip_port) {
+		return ip_ports.remove(ip_port);
+	}
 }
