b4aa53f7db34b3d628ad3c2940cfcd5bc457c4fe
Vlad Iulian Schnakovszki <shnako@yahoo.com>
2015-03-13 12:53:50 +0000

    Added mState annotation to gbpPoundBundle

--- AX2/mCom1.0.1/sourcecode/gbpbundle/gbp/converter/PoundBundle.java
+++ AX2/mCom1.0.1/sourcecode/gbpbundle/gbp/converter/PoundBundle.java
@@ -1,18 +1,20 @@
 package gbp.converter;
 
+import StateAnnotations.mState;
+import StateAnnotations.mStateType;
+import com.tunyk.currencyconverter.BankUaCom;
+import com.tunyk.currencyconverter.api.Currency;
+import com.tunyk.currencyconverter.api.CurrencyConverter;
+import com.tunyk.currencyconverter.api.CurrencyConverterException;
 import mcom.bundle.ContractType;
 import mcom.bundle.annotations.mController;
 import mcom.bundle.annotations.mControllerInit;
 import mcom.bundle.annotations.mEntity;
 import mcom.bundle.annotations.mEntityContract;
 
-import com.tunyk.currencyconverter.BankUaCom;
-import com.tunyk.currencyconverter.api.Currency;
-import com.tunyk.currencyconverter.api.CurrencyConverter;
-import com.tunyk.currencyconverter.api.CurrencyConverterException;
-
 @mController
 @mEntity
+@mState(stateType= mStateType.STATEFUL)
 public class PoundBundle {
 	//test1:returns the current value in pound for any supported currency.	 
 	static final String supportedCurrencies  = "CURRENCY: UAH,AUD,GBP,BYR,DKK,USD,EUR,NOK,CHF,CNY,JPY";
@@ -33,5 +35,4 @@ public class PoundBundle {
 	public static void main(String [] args){
 		
 	}
-	
 }
